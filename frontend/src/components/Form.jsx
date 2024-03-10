bffd928231cafec81609cd7e52791b2aed83a2c9
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2024-03-10 15:31:05 +0200

    BACKUP
    mac crapped it self and removed half of the project
    files during icloud shananigans... recovered what I could
    
    fcking shit show, wtf apple?

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -6,7 +6,7 @@
 /*   By: jmykkane <jmykkane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 13:13:37 by jmykkane          #+#    #+#             */
-/*   Updated: 2024/03/09 22:53:37 by jmykkane         ###   ########.fr       */
+/*   Updated: 2024/03/10 15:20:57 by jmykkane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,19 @@ import {
   Button,
 } from "@nextui-org/react"
 
-
-const FormInput = ({ type }) => {
-  return (
-    <div className="" >
-      <label className="fixed" htmlFor="">test</label>
-      <input style={{ fontFamily: "sans-serif" }} className="w-full h-10 border-2 rounded-xl border-blue-800 bg-blue-100" type={type} >
-      </input>
-    </div>
-  )
+const customLabel = {
+  label: 'group-data-[filled-within=true]:-translate-y-[calc(160%)] \
+          group-data-[filled-within=true]:-left-[2px] \
+          text-lg'
 }
 
 const Form = () => {
   // const payload = {}
 
   return (
-    <div className="relative flex justify-center py-4">
-      <form>
-        <Card className="w-[450px]">
+    <div className="h-full relative flex justify-center py-4">
+      <form className="h-full mt-4">
+        <Card className="w-[500px] h-3/4">
           <CardHeader className="flex justify-center p-4">
             <img
               src={f_logo}
@@ -49,15 +44,46 @@ const Form = () => {
               alt="f-secure logo"
             />
           </CardHeader>
-
           <Divider/>
-
-          <CardBody className="gap-4 p-4">
-            <FormInput type="email"/>
+          <CardBody>
+            <div className="w-full flex justify-between">
+              <Input
+                style={{ fontFamily: "sans-serif", fontSize: '20px' }}
+                classNames={customLabel}
+                labelPlacement="outside"
+                className="w-1/2 pr-2"
+                variant="bordered"
+                label="ETUNIMI"
+                type="text"
+              />
+              <Input
+                style={{ fontFamily: "sans-serif", fontSize: '20px' }}
+                classNames={customLabel}
+                labelPlacement="outside"
+                className="w-1/2 pl-2"
+                variant="bordered"
+                label="SUKUNIMI"
+                type="text"
+              />
+            </div>
+            <Input
+              style={{ fontFamily: "sans-serif", fontSize: '20px' }}
+              classNames={customLabel}
+              labelPlacement="outside"
+              variant="bordered"
+              label="EMAIL"
+              type="email"
+            />
+            <Input
+              style={{ fontFamily: "sans-serif", fontSize: '20px' }}
+              classNames={customLabel}
+              labelPlacement="outside"
+              variant="bordered"
+              label="KUITTI NRO"
+              type="text"
+            />
           </CardBody>
-
           <Divider/>
-
           <CardFooter className="justify-center p-4">
             <Button className="SubmitButton w-full" size="xl" color="secondary">
               <h1 className="text-xl">REIKISTERÖI</h1>
